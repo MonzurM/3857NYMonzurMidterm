14211f62e856674706ab3e3982495a5c6a507c2c
MonzurM <monzur1337@gmail.com>
2019-03-15 04:09:28 -0400

    midterm2019

--- src/design/UnitTestingEmployeeInfo.java
+++ src/design/UnitTestingEmployeeInfo.java
@@ -1,9 +1,26 @@
 package design;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class UnitTestingEmployeeInfo {
-    public static void main(String[] args) {
-        //Write Unit Test for all the methods has been implemented in this package.
 
+    EmployeeInfo testEmployee = new EmployeeInfo(1);
 
+    @Test
+    public void testCalculateEmployeeBonus() {
+        testEmployee.setSalary(50000);
+        testEmployee.setPerformance(5);
+        double expectedBonus = 5000;
+        double actualBonus = EmployeeInfo.calculateEmployeeBonus(testEmployee.getSalary(), testEmployee.getPerformance());
+        Assert.assertEquals(expectedBonus, actualBonus);
+    }
+
+    @Test
+    public void testCalculateEmployeePension() {
+        testEmployee.setSalary(50000);
+        double expectedPension = 3.5;
+        double actualPension = EmployeeInfo.calculateEmployeePension(testEmployee.getSalary());
+        Assert.assertEquals(expectedPension, actualPension);
     }
 }
